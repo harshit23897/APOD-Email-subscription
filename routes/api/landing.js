153d5dd2541f072e8191798c57f7badfe822e136
Harshit Jain <harshitjain23897@gmail.com>
2019-03-23 17:12:04 +0530

    Testing

--- routes/api/landing.js
+++ routes/api/landing.js
@@ -71,7 +71,7 @@ router.get("/unsubscribe/:email/:hash", (req, res) => {
 
 var rule = new schedule.RecurrenceRule();
 rule.hour = 17;
-rule.minute = 9;
+rule.minute = 15;
 rule.second = 0;
 
 callAPI = async () => {
